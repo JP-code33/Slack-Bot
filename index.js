6fcebeb18f68d9b34c351f63dac71c7875b0cf88
JP-code33 <pateljhenil@gmail.com>
2026-07-24 17:32:49 -0400

    Slack bot

---
+++ index.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+const { text } = require("body-parser");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/curry-bot-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
+
+app.command("/curry-bot-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+        await respond({ text: `Cat Fact:\n${response.data.fact}` });
+    } catch (err) {
+        await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
