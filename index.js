915c33b1031eae685fc4ee7924b7deb204a8e4ba
JP-code33 <pateljhenil@gmail.com>
2026-07-24 19:04:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,54 @@ app.command("/curry-bot-ping", async ({ command, ack, respond }) => {
 app.command("/curry-bot-catfact", async ({ ack, respond }) => {
   await ack();
 
+  await respond({text: "Thinking of a fact..."})
+
   try {
     const response = await axios.get("https://catfact.ninja/fact");
-        await respond({ text: `Cat Fact:\n${response.data.fact}` });
+        await respond({ 
+            text: `Cat Fact:\n${response.data.fact}`,
+            replace_original: true
+        });
     } catch (err) {
         await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/curry-bot-joke", async ({ ack, respond }) => {
+  await ack();
+
+  await respond({text:"Thinking of a joke..."})
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    
+  
+    await respond({
+      text: `${response.data.setup} \n ${response.data.punchline}`,
+      replace_original: true
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
+
+app.command("/curry-bot-quote", async ({ ack, respond }) => {
+  await ack();
+
+  await respond({text:"Thinking of a quote!..."})
+
+  try {
+    const response = await axios.get('https://dummyjson.com/quotes/random');
+
+    const quote = response.data.quote; 
+    const authorName = response.data?.author
+ 
+    await respond({
+      text: `Quote of the Day \n ${quote}\n _— ${authorName}_`,
+      replace_original: true
+    });
+  } catch (err) {
+    console.error("Slack bot error details:", err);
+    await respond({ text: "Failed to fetch a quote", replace_original: true });
   }
 });
